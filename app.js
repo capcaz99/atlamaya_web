fa541f6de134a0ddca833a1993f680b79f0d6123
Carlos Aburto <carlos.ab.pe@gmail.com>
2017-08-11 22:05:26 +0000

    Agregar paquetes y variables para autenticación

--- app.js
+++ app.js
@@ -1,25 +1,43 @@
-var maintenance    = require("./models/maintenance"),
-	regulation     = require("./models/regulation"),
-	financial      = require("./models/financial"),
-	payment        = require("./models/payment"),
-	methodOverride = require("method-override"),
-	table          = require("./models/table"),
-	user           = require("./models/user"),
-	ad             = require("./models/ad"),
-	bodyParser    = require("body-parser"),
-	mongoose       = require("mongoose"),
-	express        = require("express"),
-	request        = require("request"),
-	app            = express();
-	
-	
-	
-	
+var passportLocalMongoose = require("passport-local-mongoose"),
+    Maintenance 		  = require("./models/maintenance"),
+	Regulation            = require("./models/regulation"),
+	Financial   		  = require("./models/financial"),
+	Payment               = require("./models/payment"),
+	methodOverride        = require("method-override"),
+	LocalStrategy         = require("passport-local"),
+	Table                 = require("./models/table"),
+	User                  = require("./models/user"),
+	Ad                    = require("./models/ad"),
+	bodyParser            = require("body-parser"),
+	mongoose              = require("mongoose"),
+	passport              = require("passport"),
+	express               = require("express"),
+	request               = require("request"),
+	app                   = express();
 
-mongoose.Promise = global.Promise;
+
+
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+//Mongoose
+mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost/atlamaya", {useMongoClient: true});
+
+//Body Parser
+app.use(bodyParser.urlencoded({extended: true}));
+
+//Passport
+app.use(require("express-session")({
+	secret: "Every thing counts in the world",
+	resave: false,
+	saveUninitialized: false
+}));   
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
